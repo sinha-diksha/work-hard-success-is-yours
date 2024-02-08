5041e3061c1eaa0661162c7a5d0ceb06a948f915
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-02-08 22:36:16 +0530

    Time: 272 ms (17.13%) | Memory: 10.6 MB (29.72%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int numSquares(int n, vector<int>& arr) {
-        if(sqrt(n)==ceil(sqrt(n))){
-            arr[n]=1;
-            return 1;
-        }
-
-        if(arr[n]!=-1){
-            return arr[n];
-        }
-        int r=INT_MAX;
-        for(int i=1; i<=sqrt(n); i++){
-            if(n-pow(i,2)>0){
-                r=min(r, numSquares(n-pow(i,2), arr));
-            }
-        }
-
-        return arr[n]=r+1;
-    }
     int numSquares(int n) {
         vector<int> arr(n+1, -1);
-        numSquares(n, arr);
+
+        arr[0]=0;
+        arr[1]=1;
+        cout<<"asfashjfas"<<endl;
+        for(int j=2; j<=n; j++){
+            cout<<j<<endl;
+            if(sqrt(j)==ceil(sqrt(j))){
+                arr[j]=1;
+                continue;
+            }
+            int r=INT_MAX;
+            for(int i=1; i<=sqrt(j); i++){
+
+                if(j-pow(i,2)>0){
+                    r=min(r, arr[j-pow(i,2)]);
+                }
+            }
+            arr[j]=1+r;
+        }
+
         return arr[n];
     }
+   
 };
