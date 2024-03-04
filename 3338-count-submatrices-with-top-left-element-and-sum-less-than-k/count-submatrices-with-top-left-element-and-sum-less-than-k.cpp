f25f92d4891df909c5788e21a44b165589a97289
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-03-04 21:10:59 +0530

    Time: 196 ms (30.00%) | Memory: 108.9 MB (20.00%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -14,17 +14,7 @@ public:
         int ans=0;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int> > mat(n, vector<int>(m,0));
-        // mat[0][0]=grid[0][0];
-        // for(int i=1; i<m; i++){
-        //     mat[0][i]=mat[0][i-1];
-        // }
-        // for(int i=1; i<n; i++){
-        //     mat[i][0]=mat[i-1][0]+v[i][0];
-        //     for(int j=1; j<m; j++){
-        //         mat[i][j]=mat[i-1][j]+grid[i][j];
-        //     }
-        // }
+        
         for(int i=1; i<n; i++){
             for(int j=0; j<m; j++){
                 v[i][j]=v[i-1][j]+v[i][j];
