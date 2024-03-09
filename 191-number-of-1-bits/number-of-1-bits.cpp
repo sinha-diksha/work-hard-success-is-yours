8b1a1958f1ba61e378b4ce633fd0cd5c361b31b8
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-03-10 00:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (33.18%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(uint32_t n) {
         int ans=0;
         for(int i=0; i<32; i++){
-            if((n&(1<<i))!=0){
+            if(((n>>i)&1)==1){
                 ans++;
             }
         }
