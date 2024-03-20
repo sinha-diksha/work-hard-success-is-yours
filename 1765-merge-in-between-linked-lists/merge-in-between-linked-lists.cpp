ae11f9523c94cc7acdb4fb89e87e1bd026d1d221
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-03-21 00:11:52 +0530

    Time: 185 ms (37.71%) | Memory: 97.9 MB (82.89%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        int count=0;
+        ListNode* node=list1;
+        ListNode* prev=NULL;
+        ListNode* next1=NULL;
+        ListNode* del1=NULL;
+        ListNode* del2=NULL;
+        while(count!=a){
+            prev=node;
+            node=node->next;
+            count++;
+        }
+        del1=node;
+        while(count!=b){
+            node=node->next;
+      
+            count++;
+        }
+        del2=node;
+        next1=del2->next;
+        cout<<del1->val<<endl;
+        cout<<del2->val<<endl;
+        cout<<prev->val<<endl;
+        cout<<next1->val<<endl;
+        ListNode* node2=list2;
+        while(node2->next!=NULL){
+            node2=node2->next;
+        }
+        
+        del2->next=NULL;
+        prev->next=list2;
+        delete del1;
+        node2->next=next1;
+        
+        return list1;
+    }
+};
