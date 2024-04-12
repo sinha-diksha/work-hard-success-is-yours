485cc5e613be8ea2d868c37e59da8ec167969bec
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-04-12 09:40:40 +0530

    Time: 101 ms (8.01%) | Memory: 125.3 MB (5.99%) - LeetSync

--- 3374-count-alternating-subarrays/count-alternating-subarrays.cpp
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.cpp
@@ -2,26 +2,35 @@ class Solution {
     
 public:
 
- void countAlternatingSubarrays(vector<int>& nums, int i, vector<long long>& v) {
-        if(i==nums.size()-1){
-            v[i]=1;
-            return;
-        }
-        countAlternatingSubarrays(nums, i+1,v);
-        if(nums[i]==nums[i+1]){
-            v[i]=1;
+//  void countAlternatingSubarrays(vector<int>& nums, int i, vector<long long>& v) {
+//         if(i==nums.size()-1){
+//             v[i]=1;
+//             return;
+//         }
+//         countAlternatingSubarrays(nums, i+1,v);
+//         if(nums[i]==nums[i+1]){
+//             v[i]=1;
           
-        }
-        else{
-            v[i]=v[i+1]+1;
-        } 
-    }
+//         }
+//         else{
+//             v[i]=v[i+1]+1;
+//         } 
+//     }
     long long countAlternatingSubarrays(vector<int>& nums) {
         int n=nums.size();
         vector<long long> v(n);
-        countAlternatingSubarrays(nums,0,v);
-        long long ans=0;
-        for(int i=0; i<n; i++){
+        // countAlternatingSubarrays(nums,0,v);
+        v[n-1]=1;
+
+        long long ans=1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]==nums[i+1]){
+                v[i]=1;
+          
+            }
+            else{
+                v[i]=v[i+1]+1;
+            } 
             ans+=v[i];
         }
         return ans;
