fe67299a292d8a991964059ebcc195b4ef2dd2c0
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-04-15 10:40:15 +0530

    Time: 5 ms (80.00%) | Memory: 7.7 MB (60.00%) - LeetSync

---
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string findLatestTime(string s) {
+        if(s[0]=='?'){
+            if(s[1]=='?'){
+                s[0]='1';
+                s[1]='1';
+            }else{
+                if(s[1]=='1'){
+                    s[0]='1';
+                }else if(s[1]=='0'){
+                    s[0]='1';
+                }else{
+                    s[0]='0';
+                }
+            }
+        }else{
+            if(s[0]=='1'){
+                if(s[1]=='?'){
+                    s[1]='1';
+                }
+            }else if(s[0]=='0'){
+                if(s[1]=='?'){
+                    s[1]='9';
+                }
+            }
+        }
+        if(s[4]=='?'){
+            s[4]='9';
+        }
+        if(s[3]=='?'){
+            s[3]='5';
+        }
+        return s;
+    }
+};
