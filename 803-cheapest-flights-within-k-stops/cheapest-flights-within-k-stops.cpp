97b0fea3cab3d714eef5036d148782960e874d39
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-04-15 15:11:52 +0530

    Time: 22 ms (22.32%) | Memory: 17.1 MB (26.95%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,17 +2,37 @@ class Solution {
 public:
     //bellman ford.
     //just run it k+1 iterations.
-    int findCheapestPrice(int n, vector<vector<int>>& a, int src, int sink, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& a, int src, int dst, int k) {
         
-        vector<int> c(n, 1e8);
-        c[src] = 0;
-        
-        for(int z=0; z<=k; z++){
-            vector<int> C(c);
-            for(auto e: a)
-                C[e[1]] = min(C[e[1]], c[e[0]] + e[2]);
-            c = C;
+        vector<pair<int,int> > adj[n];
+        for(auto itr: a){
+            adj[itr[0]].push_back({itr[1], itr[2]});
         }
-        return c[sink] == 1e8 ? -1 : c[sink];
+        vector<int> dist(n, 1e9);
+        queue<pair<int, pair<int,int>>> q;
+        q.push({0,{src,0}});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int stops=it.first;
+            int node=it.second.first;
+            int dis=it.second.second;
+            if(stops>k){
+                continue;
+            }
+            for(auto i:adj[node]){
+                int edW=i.second;
+                int adjNode=i.first;
+                if(dis+edW < dist[adjNode] && stops<=k){
+                    dist[adjNode]=dis+edW;
+                    q.push({stops+1, {adjNode, dis+edW}});
+                }
+            }
+        }
+
+        if(dist[dst]!=1e9){
+            return dist[dst];
+        }
+        return -1;
     }
 };
