fdbbf26a24303aede4028c347e6a738f4d1f005d
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-05-29 12:20:28 +0530

    Time: 79 ms (72.44%) | Memory: 28.3 MB (72.18%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -3,16 +3,16 @@ public:
     string compressedString(string word) {
         string ans="";
         int curLen=1;
-        for(int i=1; i<word.length(); ){
+        for(int i=1; i<word.length(); i++){
             if(word[i]==word[i-1] && curLen<9){
-                i++;
+                
                 curLen++;
             }
             else if(word[i]!=word[i-1] || curLen==9){
                 ans+=to_string(curLen);
                 ans+=word[i-1];
                 curLen=1;
-                i++;
+                
             }
         }
         ans+=to_string(curLen);
