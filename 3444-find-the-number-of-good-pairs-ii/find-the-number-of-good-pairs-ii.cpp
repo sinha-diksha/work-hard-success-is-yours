7f1c6109629ce498b59bda3d12ac00e0501b0a29
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-05-30 12:43:24 +0530

    Time: 1137 ms (64.22%) | Memory: 175.4 MB (62.16%) - LeetSync

--- 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.cpp
+++ 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<int> divArray;
-        for(int i=0; i<nums1.size(); i++){
-            if(nums1[i]%k==0){
-                divArray.push_back(nums1[i]/k);
-            }
-        }
+        // vector<int> divArray;
+        // for(int i=0; i<nums1.size(); i++){
+        //     if(nums1[i]%k==0){
+        //         divArray.push_back(nums1[i]/k);
+        //     }
+        // }
         unordered_map<int,int> mp;
         for(int i=0; i<nums2.size(); i++){
             mp[nums2[i]]++;
         }
         long long ans=0;
-        for(int i=0; i<divArray.size(); i++){
-            for(int j=1; j<=sqrt(divArray[i]); j++){
-                if((divArray[i]%j)==0){
-                    int a=j;
-                    int b=divArray[i]/a;
-                    if(a==b){
-                        ans+=mp[a];
-                    }else{
-                        ans+=mp[a];
-                        ans+=mp[b];
+        for(int i=0; i<nums1.size(); i++){
+            if(nums1[i]%k==0){
+                nums1[i]/=k;
+                for(int j=1; j<=sqrt(nums1[i]); j++){
+                    if((nums1[i]%j)==0){
+                        int a=j;
+                        int b=nums1[i]/a;
+                        if(a==b){
+                            ans+=mp[a];
+                        }else{
+                            ans+=mp[a];
+                            ans+=mp[b];
+                        }
                     }
                 }
             }
