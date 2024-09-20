41bebb89fd075351be714dd81d12a77179b1e5ea
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2024-09-20 15:33:48 +0530

    Time: 4 ms (57.15%) | Memory: 23.2 MB (27.28%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        int n=s.length();
+        int i=0;
+        while(i<n){
+            if(s[i]==' '){
+                i++;
+            }else{
+                break;
+            }
+        }
+        
+        int start=i;
+        int end=i;
         string ans="";
-        int s1=0;
-        int e=0;
-        while(e<s.length()){
-            if(s[s1]==' '){
-                s1++;
-                e++;
-                
-                continue;
-            }
-            e++;
-            if(s[e]==' '){
-                if(ans==""){
-                    ans=s.substr(s1,e-s1);
-                }else{
-                    ans=s.substr(s1, e-s1)+" "+ans;
-                }
-                
-                
-                s1=e; 
-
-            }
-            if(e==s.length()){
+        while(end<n){
+            
+            if(s[end]==' '){
+                if(start!=end){
                     if(ans==""){
-                    ans=s.substr(s1,e-s1);
+                        ans=s.substr(start,end-start);
                     }else{
-                        ans=s.substr(s1, e-s1)+" "+ans;
+                        ans=s.substr(start,end-start)+" "+ans;
                     }
-                
+                }
+                end++;
+                start=end;
+            }
+            else{
+                end++;
+            }
+            if(end==n and s[end-1]!=' '){
+                if(ans==""){
+                        ans=s.substr(start,end-start);
+                    }else{
+                        ans=s.substr(start,end-start)+" "+ans;
+                    }
             }
             
         }
-
         return ans;
     }
 };
